35d216fd275d7832aa064f2274713d08e257c5fb
shubhamChouksey1 <schouksey@cognam.com>
2023-07-09 11:57:25 +0530

    updated the index file

--- forms/contact.php
+++ forms/contact.php
@@ -7,8 +7,9 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+  $receiving_email_address = 'shubhamchouksey1998@gmail.com';
 
+  
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
   } else {
@@ -24,18 +25,19 @@
   $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
+  
   $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
+    'host' => 'smtp.gmail.com',
+    'username' => 'schouksey@cognam.com',
+    'password' => 'ublfaddufsxchddo',
     'port' => '587'
   );
-  */
+  
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
   $contact->add_message( $_POST['message'], 'Message', 10);
 
   echo $contact->send();
+  
 ?>
